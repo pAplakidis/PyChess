780d2302cce12e0189447cdf44e503cf55474c3c
pAplakidis <aplakidisp@gmail.com>
2022-08-23 12:59:33 +0300

    collect data from files

--- data_proc.py
+++ data_proc.py
@@ -1,17 +1,44 @@
 #!/usr/bin/env python3
 import chess.pgn
+import os
+from tqdm import trange
+import numpy as np
+import torch
+
+data_dir = "data/"
+
+def get_data():
+  X, Y = [], []
+  results = {'1/2-1/2': 0, '0-1': 1, '1-0': 1}  # determines who won
+
+  for idx, data_file in enumerate(sorted(os.listdir(data_dir))[1:]):
+    print("(%d/%d) Processing: %s"%(idx+1, len(os.listdir(data_dir))-1, data_file))
+    pgn = open(os.path.join(data_dir, data_file))
+    while 1:
+      try:
+        game = chess.pgn.read_game(pgn)
+      except UnicodeDecodeError:
+        print("UnicodeDecodeError: skipping current file")
+        continue
+
+      if game is None:
+        break
+
+      res = game.headers['Result']
+      if res not in results:
+        continue
+
+      winner = results[res]
+      board = game.board()
+      for i, move in enumerate(game.mainline_moves()):
+        board.push(move)
+        #print(move) # TODO: need to serialize moves into a suitable tensor
+        # TODO: append to X and Y
+
+  return np.array(X), np.array(Y)
+
+
 
 if __name__ == '__main__':
-  pgn = open("data/KingBase2018/KingBase2018-01.pgn")
-  first_game = chess.pgn.read_game(pgn)
-  second_game = chess.pgn.read_game(pgn)
-
-  print("Parsing: ", first_game.headers["Event"])
-  board = first_game.board()
-  print(board)
-  print()
-  for move in first_game.mainline_moves():
-    board.push(move)
-    print(board)
-    print()
+  get_data()
 
