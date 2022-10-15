7f85879422a954e0cfbb93ecd6ba3d041f361d21
pAplakidis <user.email=aplakidisp@gmail.com>
2022-10-15 13:12:06 +0300

    more preprocessing

--- data_proc.py
+++ data_proc.py
@@ -15,6 +15,7 @@ def get_data():
   for idx, data_file in enumerate(sorted(os.listdir(data_dir))[1:]):
     print("(%d/%d) Processing: %s"%(idx+1, len(os.listdir(data_dir))-1, data_file))
     pgn = open(os.path.join(data_dir, data_file))
+    game_idx = 0
     while 1:
       try:
         game = chess.pgn.read_game(pgn)
@@ -25,26 +26,48 @@ def get_data():
       if game is None:
         break
 
+      game_idx += 1
+      print("Extracting data from game:", game_idx)
       res = game.headers['Result']
       if res not in results:
         continue
 
       winner = results[res]
       board = game.board()
+
+      n_moves = 0
+      for i, m in enumerate(game.mainline_moves()):
+        n_moves += 1
+      print("Parsing %d moves"%n_moves)
+
+      next_move = None
       for i, move in enumerate(game.mainline_moves()):
+        print("Move", i, end="\r")
         board.push(move)
-        print(move)
         serialized_board = State(board).serialize()
+        # TODO: X, Y shapes are not right
+        X.append(serialized_board)
+        Y.append(winner)
+        if i != 0:
+          next_move = move
+        if next_move is not None:
+          Y.append(next_move)
+
+        """
         print(board)
         print(serialized_board)
-        X.append(serialized_board)
-        Y.append(winner)  # TODO: maybe Y can be the next move?
-        exit(0)
+        print(move)
+        print(next_move)
+        """
+    print("%d game parsed"%game_idx)
+    break
 
   return np.array(X), np.array(Y)
 
 
 
 if __name__ == '__main__':
-  get_data()
+  X, Y = get_data()
+  print(len(X))
+  print(len(Y))
 
