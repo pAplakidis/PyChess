a8f0629818fc80a721b39f02c370926b7dd1e17d
pAplakidis <user.email=aplakidisp@gmail.com>
2022-11-20 19:10:27 +0200

    added replay memory

--- train.py
+++ train.py
@@ -13,6 +13,9 @@ from gym import spaces
 
 import gym_chess  # TODO: check this out [https://pypi.org/project/gym-chess/]
 
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
 # TODO: define the environment using gym and chess
 class Environment(gym.Env):
   # TODO: either use pygame or terminal for gameplay
@@ -28,6 +31,20 @@ class Environment(gym.Env):
       }
     )
 
+# a cyclic buffer that holds recently observed transitions
+# a transition is a tuple that maps (state, action) pairs to (next_state, reward) results
+class ReplayMemory:
+  def __init__(self, capacity):
+    self.memory = deque([], maxlen=capacity)
+
+  def push(self, *args):
+    self.memory.append(Transition(*args))
+
+  def sample(self, batch_size):
+    reutrn random.sample(self.memory, batch_size)
+
+  def __len__(self):
+    return len(self.memory)
 
 # TODO: define the DQN agent
 class Agent:
